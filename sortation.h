b11a28335ae3755c6ad98e60a517bedc13b9be51
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2023-05-25 09:57:29 +0100

    Add files via upload

--- sortation.h
+++ sortation.h
@@ -1,5 +1,20 @@
 #pragma once
 #include "Fuzzer.h"
+#include <concurrent_vector.h>;
+#include <concurrent_unordered_map.h>
+
+
+int count = 0;
+struct clockCount {
+	clockCount() {
+		count++;
+	}
+	~clockCount() {
+    
+    
+    };
+};
+
 
 struct datasortType {
     char characters = 'a' - 'Z';
@@ -9,11 +24,26 @@ struct datasortType {
     datasortType() {
         std::lock_guard<std::mutex> lock(responseMutex);
         for (const auto& pair : urlResponses) {
-            std::string url = pair.first;
-            std::string response = pair.second;
-            std::sort(response.begin(), response.end());
-            std::cout << "This is the response parsed: " + response + "\n" << std::endl;
+            url = pair.first;
+            response = pair.second;
+            std::cout << "This is the response stored: " + response + "\n" << std::endl;
+            URL.push_back(url);
+            UrlResponses_requests[url] = response;//stores the key into the headers local map
+            Response.push_back(response);
         }
+       urlResponses.clear();//urlresponse is cleared as its better to store in one place at a time.
+
+
     }
     ~datasortType() {}
+
+    private: 
+        clockCount cc;
+        std::string url;
+        std::string response;
+        Concurrency::concurrent_vector<std::string> URL;
+        Concurrency::concurrent_vector<std::string> Response;
+        Concurrency::concurrent_unordered_map<std::string, std::string> UrlResponses_requests;//change for ordered storage of url+responses.
 };
+
+
