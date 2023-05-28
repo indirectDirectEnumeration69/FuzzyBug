4a2de92fe5c0d138ddc3a7a1aaa4b7bb882bb7d6
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2023-05-28 05:15:28 +0100

    Add files via upload

--- Kern.h
+++ Kern.h
@@ -33,7 +33,6 @@ public:
         si.cb = sizeof(si);
         ZeroMemory(&pi, sizeof(pi));
 
-        // Start the child process. 
         std::wstring wProcessName = std::wstring(processName.begin(), processName.end());
         if (!CreateProcess(
             NULL,                          
@@ -51,7 +50,6 @@ public:
             printf("CreateProcess failed (%d).\n", GetLastError());
             return;
         }
-        // Resume the child process.
         if (ResumeThread(pi.hThread) == -1) {
             printf("ResumeThread failed (%d).\n", GetLastError());
             return;
