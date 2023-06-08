85c24b810f342cba7fe4411c5e7fef0771b03bd5
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2023-06-08 18:42:32 +0100

    Add files via upload

--- SystemEncryption.h
+++ SystemEncryption.h
@@ -1,22 +1,124 @@
 #pragma once
 #include "SystemKeyLogic.h"
+#include <unordered_map>
+#include <random>
+#include <chrono>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <algorithm>
 
 class Keys {
 public:
-    bool keys[1024] = { false };
-	
-	Keys() {
 
-	}
-	~Keys() {
+    std::unordered_map<int, long long> keys;
+    std::unordered_map<int, std::chrono::system_clock::time_point> keyTimestamps;
+    static const int KEY_EXPIRATION_MINUTES = 60;
 
+    Keys() = default;
+    ~Keys() = default;
 
-	}
+    void setKey(int key);
+    long long getKey(int key) const;
+    bool isKeySet(int key) const;
+    bool isKeyExpired(int key) const;
 
-	void setKey(int key, bool state);
-	bool getKey(int key);
-
-private: 
-
+private:
+    long long generateRandomNumber();
 };
 
+class KeyLogic {
+public:
+
+    void setKeys(int key) {
+
+        UseKeys.setKey(key);
+    }
+
+    long long getKey(int key) {
+        return UseKeys.getKey(key);
+    }
+
+    bool isKeySet(int key) {
+        return UseKeys.isKeySet(key);
+    }
+
+    bool isKeyExpired(int key) {
+        return UseKeys.isKeyExpired(key);
+    }
+
+    void setPassword(const std::string& password) {
+        this->password = password;
+    }
+
+    bool checkPassword(const std::string& password) const {
+        return this->password == password;
+    }
+    void generateSessionID() {
+        auto now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> distr(0, 999999999);
+        std::array<int, 4> random_numbers;
+        for (int& num : random_numbers) {
+            num = distr(gen);
+        }
+        std::string str("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?<>/~][^¬|'!@#$-%^&*()_+/`'':");
+        std::shuffle(str.begin(), str.end(), gen);
+        std::string random_string = str.substr(0, 33);
+
+        std::ostringstream oss;
+        oss << now;
+
+        for (const int& num : random_numbers) {
+            oss << "_" << num;
+        }
+
+        oss << "_" << random_string;
+
+        sessionID = oss.str();
+    }
+
+    std::string getSessionID() const {
+
+        return sessionID;
+    }
+
+private:
+    Keys UseKeys;
+    std::string password;
+    std::string sessionID;
+};
+
+class checkintegration {
+public:
+    checkintegration() {
+    if (!Key.isKeySet(1)) {
+        Key.setKeys(1);
+    }
+
+    long long keyValue = Key.getKey(1);
+
+    if (keyValue == 0 || Key.isKeyExpired(1)) {
+        KeyValid = false;
+        KeyIntegrated = false;
+    }
+    else if(keyValue != 0 && (Key.getSessionID() != "" && Key.getSessionID().size() > 20)){
+        KeyValid = true;
+        KeyIntegrated = true;
+    }
+}
+    bool isKeyValid() const {
+        return KeyValid;
+    }
+
+    bool isKeyIntegrated() const {
+        return KeyIntegrated;
+    }
+private:
+    bool KeyValid = false;
+    bool KeyIntegrated = false;
+    KeyLogic Key;
+};
--- SystemKeyLogic.h
+++ SystemKeyLogic.h
@@ -1,11 +1,50 @@
 #pragma once
 #include "SystemEncryption.h"
+#include <chrono>
 
-void Keys::setKey(int key, bool state)
+long long Keys::generateRandomNumber()
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<long long> distr(1, 999199299923LL);
+
+    return distr(gen);
 }
 
-bool Keys::getKey(int key)
+void Keys::setKey(int key)
 {
-	return false;
+    keys[key] = generateRandomNumber();
+    keyTimestamps[key] = std::chrono::system_clock::now();
+}
+
+long long Keys::getKey(int key) const
+{
+    if (keys.find(key) != keys.end())
+    {
+        return keys.at(key);
+    }
+    else
+    {
+        throw std::out_of_range("Key not found");
+    }
+}
+
+bool Keys::isKeySet(int key) const
+{
+    return keys.count(key) > 0;
+}
+
+bool Keys::isKeyExpired(int key) const
+{
+    if (keys.find(key) != keys.end())
+    {
+        auto now = std::chrono::system_clock::now();
+        auto keyTime = keyTimestamps.at(key);
+        auto duration = std::chrono::duration_cast<std::chrono::minutes>(now - keyTime);
+        return duration.count() > KEY_EXPIRATION_MINUTES;
+    }
+    else
+    {
+        throw std::out_of_range("Key not found");
+    }
 }
